128a404022f49e706a55e44bf970162c0a93f039
arturmuursepp <artur.ateam@gmail.com>
2024-05-22 10:11:45 +0300

    created Employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiCRUDmyyrsepp.Models
+{
+    internal class Employee
+    {
+    }
+}
